1877219f5aa5762436a38e8d68a1c516cd926efb
John Dinh <johnduydinh@gmail.com>
2019-10-04 19:59:25 -0400

    Complete Section 11 - Lesson 107

---
+++ Section 11. JavaScript/Lesson 107. Exercise - Make A Calculator/script.js
@@ -0,0 +1,8 @@
+let num1, num2, sum;
+
+num1 = Number(prompt("Enter a number"));
+num2 = Number(prompt("Enter your second number"));
+
+sum = num1 + num2;
+
+alert(`The sum of those two numbers is ${sum}`);
